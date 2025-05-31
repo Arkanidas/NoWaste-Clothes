e1ae043c01f91e1c1371b954424a53f49c4d3063
Leon <xleonidasprox@gmail.com>
2025-05-31 22:52:50 +0200

    nav fixing

--- components /Navbar.tsx
+++ components /Navbar.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
-import styles from "../components /navbar.module.css"
+import Image from "next/image";
+import styles from "../components /navbar.module.css";
+import logo from '../public/nowaste logo.png'
+
+  // <Image src={logo} alt="NoWaste Logo" className={styles.logo} width={10} height={10}/>
 
 export default function Navbar() {
   return (
-    <nav className={styles.Navigation}>
-      <ul className={styles.Border}>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/About">About us</Link></li>
-        <li><Link href="/Contact">Contact</Link></li>
+    <div className={styles.Navigation}>
+      <ul className={styles.navlist}>
+        
+        <li className="links"><Link href="/">Home</Link></li>
+        <li className="links"><Link href="/About">About us</Link></li>
+        <li className="links"><Link href="/Contact">Contact</Link></li>
       </ul>
-    </nav>
+    </div>
   );
 }

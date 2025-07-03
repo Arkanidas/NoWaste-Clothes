ae21fff5a2637c678f8efa9ac6a83135b713e753
Leon <xleonidasprox@gmail.com>
2025-07-03 23:08:56 +0200

    about us text change

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -6,9 +6,7 @@ export default function About() {
        <h3 className={styles.header}>Om oss</h3>
        <img className={styles.image} src="/about.png" alt="About Us" />
       <p className={styles.description}>
-        Teamet bakom Nowaste består av passionerade individer som brinner för hållbarhet och att hjälpa andra. 
-        Vi har arbetat med detta projekt sedan början av 2020 och har som mål att göra det enkelt för människor att donera sina begagnade kläder, möbler och andra föremål till behövande. 
-        Genom vårt initiativ strävar vi efter att minska avfall och samtidigt hjälpa samhällen i Afrika och Ukraina att få tillgång till resurser de annars saknar.
+        Idel Förening grundades 
       </p>
     </>
   );

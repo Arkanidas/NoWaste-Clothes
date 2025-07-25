7eb00214a461bc5cc39bce4ff76dd69c286f7db6
Leon <xleonidasprox@gmail.com>
2025-07-25 23:57:47 +0200

    about us info

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -6,7 +6,11 @@ export default function About() {
        <h3 className={styles.header}>Om oss</h3>
        <img className={styles.image} src="/about.png" alt="About Us" />
       <p className={styles.description}>
-        Idel Förening grundades 2019 aha idell industri i foollow 
+        Idel Förening grundades år 2019 med ett tydligt syfte: att hjälpa människor i nöd runt om i världen.
+        Sedan starten har vi engagerat oss i att samla in och donera kläder, hushållsartiklar och andra förnödenheter till utsatta områden,
+        särskilt där krig, fattigdom och humanitära kriser har skapat stora behov.
+        <br></br>
+        Vi arbetar
       </p>
     </>
   );
